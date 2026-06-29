48861b0bdd044be9059284da9f42f9b14b18ad14
Kodali Mahesh <kodalidurgamahesh5@gmail.com>
2026-06-29 23:30:39 +0530

    Day 4 Runner movement from left to right.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,28 +2,21 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public Rigidbody rb;
+    public float Forwardspeed = 2000f;
+    public float sidewayspeed = 500f;
     public float speed = 5f;
     void Update()
     {
-        if (Input.GetKey(KeyCode.W))
-        {
-            Debug.Log("Player is moving forward");
-            transform.position += Vector3.forward * speed * Time.deltaTime;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            Debug.Log("Player is moving backward");
-            transform.position += Vector3.back * speed * Time.deltaTime;
-        }
+        rb.AddForce(0, 0, Forwardspeed * Time.deltaTime);
+
         if (Input.GetKey(KeyCode.A))
         {
-            Debug.Log("Player is moving left");
-            transform.position += Vector3.left * speed * Time.deltaTime;   
+            rb.AddForce(-sidewayspeed * Time.deltaTime, 0, 0);
         }
         if (Input.GetKey(KeyCode.D))
         {
-            Debug.Log("Player is moving right");
-            transform.position += Vector3.right * speed * Time.deltaTime;
+            rb.AddForce(sidewayspeed * Time.deltaTime, 0, 0);
         }    
     }
 }
